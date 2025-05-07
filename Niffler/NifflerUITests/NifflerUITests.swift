e2fb287da669f6b2b71ef6df5af19fbc719e67b8
dilook <udenav@yandex.ru>
2025-05-07 11:46:56 +0400

    add: ui test for signup

--- Niffler/NifflerUITests/NifflerUITests.swift
+++ Niffler/NifflerUITests/NifflerUITests.swift
@@ -9,33 +9,39 @@ import XCTest
 
 final class NifflerUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testSignUp() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        app.staticTexts["Create new account"].tap()
+        let userNameInput = app.textFields.matching(identifier: "userNameTextField").element(boundBy: 0)
+        userNameInput.tap()
+        let userName = UUID().uuidString
+        userNameInput.typeText(userName)
+        let pwd = app.secureTextFields.matching(identifier: "passwordTextField").element(boundBy: 0)
+        pwd.tap()
+        pwd.typeText("12345")
+        app.secureTextFields["confirmPasswordTextField"].tap()
+        app.secureTextFields["confirmPasswordTextField"].typeText("12345")
+        app.buttons["Sign Up"].tap()
+        XCTAssertTrue(app.staticTexts["Congratulations!"].isHittable)
+        XCTAssertTrue(app.staticTexts[" You've registered!"].isHittable)
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+    
+    func testTransferCredentialsToRegisterScreen() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let userName = UUID().uuidString
+        app.textFields["userNameTextField"].tap()
+        app.textFields["userNameTextField"].typeText(userName)
+        app.secureTextFields["passwordTextField"].tap()
+        app.secureTextFields["passwordTextField"].typeText("12345")
+        app.staticTexts["Create new account"].tap()
+        
+        XCTAssertEqual(app.textFields.matching(identifier: "userNameTextField").element(boundBy: 0).value as? String, userName)
+        app.buttons.matching(identifier: "passwordTextField").element(boundBy: 0).tap()
+        XCTAssertEqual(app.textFields["passwordTextField"].value as? String, "12345")
     }
 }
